194a2c31fc66ec622924180db68ec1e4e5576115
Hardy Jimenez <hardy.jimenez.matos@accenture.com>
2022-10-25 15:26:59 +0200

    fix(config): fix notify error with packerSync

--- config.lua
+++ config.lua
@@ -69,8 +69,6 @@ lvim.builtin.alpha = {
   active = true,
   mode = "dashboard",
 }
-lvim.builtin.notify.active = true
-lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
 lvim.builtin.nvimtree.setup.view.width = 55
