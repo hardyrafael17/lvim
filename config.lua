2e2b85fcd9309118e75be5f69ec00eaf307d4ca5
Hardy Jimenez <hardy.jimenez.matos@accenture.com>
2022-10-25 15:03:29 +0200

    chore(config.lua): removed deprecated option
    removed notify options

--- config.lua
+++ config.lua
@@ -70,7 +70,6 @@ lvim.builtin.alpha = {
   mode = "dashboard",
 }
 lvim.builtin.notify.active = true
-lvim.builtin.notify.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
