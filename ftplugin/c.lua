fd5e659df8ee9be0b5215415358f23e264a54182
Hardy Rafael Jimenez Matos <hardy.jimenez.matos@accenture.com>
2022-12-07 20:16:43 +0100

    chore(config-lua): update config
    update my config from linux

--- ftplugin/c.lua
+++ ftplugin/c.lua
@@ -7,6 +7,6 @@ opt.colorcolumn = '80'      -- Line lenght marker at 80 columns
 -- Tabs, indent
 -----------------------------------------------------------
 opt.expandtab = false        -- Use spaces instead of tabs
-opt.tabstop = 4             -- 1 tab == 4 spaces
 opt.smartindent = true      -- Autoindent new lines
+opt.shiftwidth = 2
 
